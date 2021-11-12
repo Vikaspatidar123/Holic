aaf5e27c719f7b5879138f180e61547d04d68baa
vikash <apextrainee0002@gmail.com>
2021-11-12 11:01:30 +0530

    first commit

--- tea/settings.py
+++ tea/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cxmy&ze_q-7&0py+s646bzim8@2p5*xs^dy#h%qq-!3p+y)g^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
